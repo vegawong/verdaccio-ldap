b99eec2a20351323770775924bcf65ecd064fe7e
vega <vegawong1024@gmail.com>
2018-09-12 09:46:43 +0800

    fix: 优化ldap链接

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const Promise = require('bluebird');
-const rfc2253 = require('rfc2253');
-const LdapAuth = require('ldapauth-fork');
+const Promise = require("bluebird");
+const rfc2253 = require("rfc2253");
+const LdapAuth = require("ldapauth-fork");
 
 Promise.promisifyAll(LdapAuth.prototype);
 
@@ -20,7 +20,7 @@ function Auth(config, stuff) {
   self.cacheTime = config.cacheTime || 1000 * 60 * 3;
 
   // TODO: Set more defaults
-  self._config.groupNameAttribute = self._config.groupNameAttribute || 'cn';
+  self._config.groupNameAttribute = self._config.groupNameAttribute || "cn";
 
   return self;
 }
@@ -30,37 +30,52 @@ module.exports = Auth;
 //
 // Attempt to authenticate user against LDAP backend
 //
-Auth.prototype.authenticate = function (user, password, callback) {
+Auth.prototype.authenticate = function(user, password, callback) {
+  const cacheUser = this.getCacheUser(user);
+  if (cacheUser) {
+    this._logger.trace(
+      {
+        user,
+        cacheUser
+      },
+      `Ldap --- find out cache user: ${user}`
+    );
+    return callback(null, [
+      cacheUser.cn,
+      ...(cacheUser._groups
+        ? [].concat(cacheUser._groups).map(group => group.cn)
+        : []),
+      ...(cacheUser.memberOf
+        ? []
+            .concat(cacheUser.memberOf)
+            .map(groupDn => rfc2253.parse(groupDn).get("CN"))
+        : [])
+    ]);
+  }
+
+  this._logger.trace(
+    {
+      user
+    },
+    `Ldap --- miss cache user: ${user}`
+  );
+
+  this._logger.trace(
+    {
+      user: user
+    },
+    `Ldap --- user:${user} generate a request ti ldap server to authenticate`
+  );
+
   const LdapClient = new LdapAuth(this._config.client_options);
 
   // https://github.com/vesse/node-ldapauth-fork/issues/61
-    LdapClient.on('error', (err) => {
-        this._logger.error({err}, `Ldap --- ldapClient is crash, ${err}`);
-    });
-
-    const cacheUser = this.getCacheUser(user);
-    if(cacheUser) {
-        this._logger.trace({
-            user,
-            cacheUser,
-        }, `Ldap --- find out cache user: ${user}`);
-        return callback(null, [
-            cacheUser.cn,
-            ...cacheUser._groups ? [].concat(cacheUser._groups).map((group) => group.cn) : [],
-            ...cacheUser.memberOf ? [].concat(cacheUser.memberOf).map((groupDn) => rfc2253.parse(groupDn).get('CN')) : [],
-        ]);
-    }
-
-    this._logger.trace({
-        user
-    }, `Ldap --- miss cache user: ${user}`);
-
-    this._logger.trace({
-        user: user
-    }, `Ldap --- user:${user} generate a request ti ldap server to authenticate`);
+  LdapClient.on("error", err => {
+    this._logger.error({ err }, `Ldap --- ldapClient is crash, ${err}`);
+  });
 
   LdapClient.authenticateAsync(user, password)
-    .then((ldapUser) => {
+    .then(ldapUser => {
       if (!ldapUser) return [];
 
       this.setCacheUser(user, ldapUser);
@@ -68,62 +83,91 @@ Auth.prototype.authenticate = function (user, password, callback) {
       return [
         ldapUser.cn,
         // _groups or memberOf could be single els or arrays.
-        ...ldapUser._groups ? [].concat(ldapUser._groups).map((group) => group.cn) : [],
-        ...ldapUser.memberOf ? [].concat(ldapUser.memberOf).map((groupDn) => rfc2253.parse(groupDn).get('CN')) : [],
+        ...(ldapUser._groups
+          ? [].concat(ldapUser._groups).map(group => group.cn)
+          : []),
+        ...(ldapUser.memberOf
+          ? []
+              .concat(ldapUser.memberOf)
+              .map(groupDn => rfc2253.parse(groupDn).get("CN"))
+          : [])
       ];
     })
-    .catch((err) => {
+    .catch(err => {
       // 'No such user' is reported via error
       this.removeCacheUser(user);
-      this._logger.warn({
-        user: user,
-        err: err,
-      }, `LDAP error ${err}`);
+      this._logger.warn(
+        {
+          user: user,
+          err: err
+        },
+        `LDAP error ${err}`
+      );
 
       return false; // indicates failure
     })
-    .finally((ldapUser) => {
+    .finally(ldapUser => {
       LdapClient.closeAsync()
-        .catch((err) => {
-          this._logger.warn({
-            err: err
-          }, `LDAP error on close ${err}`);
+        .then(() => {
+          this._logger.trace(
+            {
+              user
+            },
+            `LDAP --- ${user}'s request is closed `
+          );
+        })
+        .catch(err => {
+          this._logger.warn(
+            {
+              err: err
+            },
+            `LDAP error on close ${err}`
+          );
         });
       return ldapUser;
     })
     .asCallback(callback);
 };
 
-Auth.prototype.getCacheUser = function (key) {
-    const cacheUser = this._users[key];
-    if(!cacheUser) {
-        return null;
-    }
-    if(Date.now() > cacheUser.expiredTime) {
-        this._logger.trace({
-            user: key
-        }, `Ldap --- user: ${key} is outed of expiredTime`);
-        this.removeCacheUser(key);
-        return null;
-    }
-    return cacheUser.data;
-};
-
-Auth.prototype.setCacheUser = function (key, data) {
-    this._logger.trace({
-        user: key,
-        data
-    }, `Ldap cache set: ${key}`);
-    const cacheUser = {
-        data,
-        expiredTime: Date.now() + this.cacheTime
-    };
-    this._users[key] = cacheUser;
-};
-
-Auth.prototype.removeCacheUser = function (key) {
-    this._logger.trace({
+Auth.prototype.getCacheUser = function(key) {
+  const cacheUser = this._users[key];
+  if (!cacheUser) {
+    return null;
+  }
+  if (Date.now() > cacheUser.expiredTime) {
+    this._logger.trace(
+      {
         user: key
-    }, `Ldap --- remove cache user: ${key}`);
-    delete this._users[key];
+      },
+      `Ldap --- user: ${key} is outed of expiredTime`
+    );
+    this.removeCacheUser(key);
+    return null;
+  }
+  return cacheUser.data;
+};
+
+Auth.prototype.setCacheUser = function(key, data) {
+  this._logger.trace(
+    {
+      user: key,
+      data
+    },
+    `Ldap cache set: ${key}`
+  );
+  const cacheUser = {
+    data,
+    expiredTime: Date.now() + this.cacheTime
+  };
+  this._users[key] = cacheUser;
+};
+
+Auth.prototype.removeCacheUser = function(key) {
+  this._logger.trace(
+    {
+      user: key
+    },
+    `Ldap --- remove cache user: ${key}`
+  );
+  delete this._users[key];
 };
